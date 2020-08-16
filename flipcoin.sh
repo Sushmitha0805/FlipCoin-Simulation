d4451d4e1d0c9079ed440770881dd766c7b90939
Sushmitha0805 <rsushmitha822@gmail.com>
2020-08-16 12:59:14 +0530

    added uc2

--- flipcoin.sh
+++ flipcoin.sh
@@ -10,3 +10,24 @@ then
 else
 	echo " Flip coin is Tails "
 fi
+
+
+
+while [ $valid ]
+   RANDOM_CHECK=$((RANDOM%2))
+do
+ 	if [ $RANDOM_CHECK -eq $IS_HEAD ]
+   then
+      	HEAD_CNT=$(( $HEAD_CNT+1 ))
+
+	else
+      	TAIL_CNT=$(( $TAIL_CNT+1 ))
+   fi
+		if [[ $HEAD_CNT -eq 21 || $TAIL_CNT -eq 21 ]]
+   	then
+      	break
+   	fi
+
+done
+      echo " Heads wins "$HEAD_CNT" times "
+      echo " Tails wins "$TAIL_CNT" times "
