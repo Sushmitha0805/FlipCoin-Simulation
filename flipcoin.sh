8e52e35f658928e83c89bef814c1c8b31d54ade1
Sushmitha0805 <rsushmitha822@gmail.com>
2020-08-16 12:54:21 +0530

    added uc1

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "flip coin simulation"
+IS_HEAD=1;
+RANDOM_CHECK=$((RANDOM%2))
+
+if [ $RANDOM_CHECK -eq $IS_HEAD ]
+then
+	echo " Flip coin is Heads "
+else
+	echo " Flip coin is Tails "
+fi
